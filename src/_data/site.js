d7b042ca2101c4b6a63037219b5783990b7561c8
John Sieber <john@john-sieber.com>
2021-03-18 21:53:37 -0600

    move site title to global var

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,5 +2,6 @@
 
 module.exports = {
   tagline: 'Our online recipe collection',
-  copyrightYear: (new Date()).getFullYear()
+  copyrightYear: (new Date()).getFullYear(),
+  title: 'I\'m In The Mood For Food'
 }
